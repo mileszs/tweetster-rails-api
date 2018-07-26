fb1f245efadf5080bd6ed949d9b24bbfed5a8574
Miles Z. Sterrett <miles@mileszs.com>
2018-07-26 16:29:15 -0400

    Provide a couple Tweets to seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Tweet.create(
+  username: "vegeta",
+  first_name: "Vegeta",
+  last_name: "Saiyan prince",
+  body: "IT'S OVER 9000!"
+)
+
+Tweet.create(
+  username: "goku",
+  first_name: "Goku",
+  last_name: "Kakarot",
+  body: "KAMEHAMEHA!!!!!"
+)

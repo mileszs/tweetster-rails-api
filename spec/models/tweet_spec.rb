2318b3514d58a33217e2c26b9e1bbf59d16aa2b3
Boberkraft <andrzej.bisewski@gmail.com>
2020-02-07 13:22:24 +0100

    d

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Tweet do
   context 'Stary sypiacy sie test' do
     it { expect(49).to eq("beka") }
   end
+
+
+  context 'Nowy sypiacy sie test' do
+    it { expect(49).to eq("beka") }
+  end
 end

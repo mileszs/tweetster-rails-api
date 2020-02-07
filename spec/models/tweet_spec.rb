2f9a7738c1c4f5b6e6ee0d73b37f13f0fa5c6c59
Boberkraft <andrzej.bisewski@gmail.com>
2020-02-07 13:12:37 +0100

    dd

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -2,17 +2,7 @@ require 'rails_helper'
 
 
 RSpec.describe Tweet do
-  context '' do
-    it { expect(1).to eq(1) }
-  end
-
-
-  context '' do
-    it { expect(1).to eq(2) }
-  end
-
-  context 'nowy sypiacy sie test' do
+  context 'Stary sypiacy sie test' do
     it { expect(49).to eq("beka") }
   end
-
 end
